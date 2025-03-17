e07eb8fef0faff125364c6925e431e37ac2a9f00
Coderyounes <younesanas2021@gmail.com>
2025-03-17 14:07:33 +0000

    create service model & add relationship to belongto my_companie

---
+++ backend/app/Models/service.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class service extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'my_companie_id'];
+
+    public function company()
+    {
+        return $this->belongsTo(MyCompany::class);
+    }
+}

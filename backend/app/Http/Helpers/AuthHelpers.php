bde3d31327576372e895228c0eeb6cf5120aae94
Coderyounes <younesanas2021@gmail.com>
2025-03-17 12:16:48 +0000

    create a Class Helper to extract the UserId or the CompanyId

---
+++ backend/app/Http/Helpers/AuthHelpers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Helpers;
+
+use App\Models\MyCompany;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class AuthHelpers
+{
+
+    public static function getId(string $token) {
+        $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        return $decoded->sub;
+    }
+
+
+
+    public  static function  getMyCompany(string $token) {
+        $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $userId = $decoded->sub;
+        return MyCompany::where('owner_id', $userId)->first();
+    }
+
+}

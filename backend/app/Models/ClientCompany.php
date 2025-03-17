fdbc489276073663a50c34a4c5058af2e5fbaf07
Coderyounes <younesanas2021@gmail.com>
2025-03-17 12:12:13 +0000

    create client_company model & define the relationship with company & contacts

---
+++ backend/app/Models/ClientCompany.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientCompany extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'industry',
+        'address',
+        'phone',
+        'email',
+        'image',
+        'my_companie_id'
+    ];
+
+    public function company() {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function contacts() {
+        return $this->hasMany(Contact::class);
+    }
+}

ce1095f277f036af1c745771131442055efcc8f5
Coderyounes <younesanas2021@gmail.com>
2025-03-17 12:05:19 +0000

    create a table to store the invited users token & some data

---
+++ backend/database/migrations/2025_03_16_130841_create_invites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->nullable(false);
+            $table->string('token')->nullable(false);
+            $table->boolean('is_used')->default(false);
+            $table->enum('role_id', [2, 3])->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invites');
+    }
+};

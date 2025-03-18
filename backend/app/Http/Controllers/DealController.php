9717fbebb8271ec884adfd37fda1fed326817724
Coderyounes <younesanas2021@gmail.com>
2025-03-18 22:50:43 +0000

    create a function we can unassing deal from the agent

--- backend/app/Http/Controllers/DealController.php
+++ backend/app/Http/Controllers/DealController.php
@@ -136,5 +136,20 @@ class DealController extends Controller
         ]);
     }
 
-    public function unassign() {}
+    public function unassign(Request $request, string $dealId) {
+        $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
+        try {
+            $deal = Deal::where('my_companie_id', $companyId)->where('id', $dealId)->first();
+            if(!$deal) {
+                return response()->json(['message' => 'Deal not found'], 404);
+            }
+            $deal->update(['agent_id' => null]);
+        } catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+
+        return response()->json([
+            'message' => $deal
+        ]);
+    }
 }

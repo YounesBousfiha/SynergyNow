e61c2d2292b4bc9fed15ae1f8d7fe9a0c79bd900
Coderyounes <younesanas2021@gmail.com>
2025-03-27 00:52:15 +0000

    finalise les gestion des taches controller

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -30,30 +30,100 @@ class TaskController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
-        return response()->json(['message' => 'Task created successfully!'], 201);
+        return response()->json($task, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show(Request $request, string $taskId)
     {
-        //
+        try {
+            $userId = AuthHelpers::getId($request->bearerToken());
+            $task = Task::where('user_id', $userId)->orWhere('assigned_to', $userId)->where('id', $taskId)->first();
+            if (!$task) {
+                return response()->json(['message' => 'Task not found!'], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+        return response()->json($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, string $taskId)
     {
-        //
+        try {
+            $userId = AuthHelpers::getId($request->bearerToken());
+            $task = Task::where('user_id', $userId)->where('id', $taskId)->first();
+            if (!$task) {
+                return response()->json(['message' => 'Task not found!'], 404);
+            }
+            $task->update($request->all());
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+        return response()->json($task);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy(string $taskId)
     {
-        //
+        try {
+            $task = Task::find($taskId);
+            if (!$task) {
+                return response()->json(['message' => 'Task not found!'], 404);
+            }
+            $task->delete();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+
+        return response()->json(['message' => 'Task deleted successfully!']);
+    }
+
+    public function assigneeTo(Request $request, string $taskId)
+    {
+        try {
+            $userId = AuthHelpers::getId($request->bearerToken());
+            $task = Task::where('user_id', $userId)->where('id', $taskId)->first();
+            if (!$task) {
+                return response()->json(['message' => 'Task not found!'], 404);
+            }
+            $task->update(['assigned_to' => $request->assigned_to]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+        return response()->json($task);
+    }
+
+    public function unassign(Request $request, string $taskId)
+    {
+        try {
+            $userId = AuthHelpers::getId($request->bearerToken());
+            $task = Task::where('user_id', $userId)->where('id', $taskId)->first();
+            if (!$task) {
+                return response()->json(['message' => 'Task not found!'], 404);
+            }
+            $task->update(['assigned_to' => null]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+        return response()->json($task);
+    }
+
+    public function myTasks(Request $request)
+    {
+        try {
+            $userId = AuthHelpers::getId($request->bearerToken());
+            $tasks = Task::where('assigned_to', $userId)->get();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+        return response()->json($tasks);
     }
 }

2c1393d1ac0dcdcbce9df06fdd0508eb5e14b615
Coderyounes <younesanas2021@gmail.com>
2025-03-27 00:46:38 +0000

    update task model fillable

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'start_date', 'due_date', 'status', 'priority', 'user_id'];
+    protected $fillable = ['title', 'description', 'start_date', 'due_date', 'status', 'priority', 'user_id', 'assigned_to'];
 
     public function user()
     {

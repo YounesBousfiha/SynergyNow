776755aca02ea8583bacbcfb21394d5818f01be1
Coderyounes <younesanas2021@gmail.com>
2025-04-02 13:06:05 +0000

    fix the relatinship with quote & other models

--- backend/app/Models/Quote.php
+++ backend/app/Models/Quote.php
@@ -11,10 +11,14 @@ class Quote extends Model
     // TODO: have a many to many relationship with  services
     // TODO: Update the columns to have like total, status, notes, date D'experation
     public function service() {
-        return $this->belongsTo(Service::class);
+        return $this->belongsTo(Service::class, 'service_id');
+    }
+
+    public function deal() {
+        return $this->belongsTo(Deal::class, 'deal_id');
     }
 
     public function clientCompany() {
-        return $this->belongsTo(ClientCompany::class, 'client_company_id');
+        return $this->belongsTo(ClientCompany::class, 'client_companies_id');
     }
 }

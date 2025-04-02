474b2fac18a36ce49478811ed823b944a2c07b05
Coderyounes <younesanas2021@gmail.com>
2025-04-02 12:01:28 +0000

    update the Quote fillable

--- backend/app/Models/Quote.php
+++ backend/app/Models/Quote.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
-    protected $fillable = [];
+    protected $fillable = ['title', 'deal_id', 'service_id', 'client_companies_id'];
 
-
+    // TODO: have a many to many relationship with  services
+    // TODO: Update the columns to have like total, status, notes, date D'experation
     public function service() {
         return $this->belongsTo(Service::class);
     }

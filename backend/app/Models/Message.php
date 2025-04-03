8b2705c136fc675dd096378dba6b7f4a8ccb8a79
Coderyounes <younesanas2021@gmail.com>
2025-04-03 00:33:59 +0000

    use the attachements trait in Message Model

--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\HasAttachments;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasAttachments;
+
+    protected $table = 'messages';
     protected $fillable = [
         'chat_id',
         'sender_id',

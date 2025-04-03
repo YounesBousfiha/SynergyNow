081eeac8873de10b594138e2a3fa63517737d34e
Coderyounes <younesanas2021@gmail.com>
2025-04-03 00:34:05 +0000

    use the attachements trait in Note Model

--- backend/app/Models/Note.php
+++ backend/app/Models/Note.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Traits\HasAttachments;
 use Illuminate\Database\Eloquent\Model;
 
+
+// TODO:
 class Note extends Model
 {
+    use HasAttachments;
+
+    protected $table = 'notes';
+
     protected  $fillable = ['content', 'user_id', 'contact_id'];
 
     public function users()

5afea30b50c7a902827bccd2ba091a98c59d3fe9
Coderyounes <younesanas2021@gmail.com>
2025-04-09 16:38:11 +0100

    Add input validation for registration form fields

--- frontend/src/app/(public)/register/_componenets/RegisterForm.jsx
+++ frontend/src/app/(public)/register/_componenets/RegisterForm.jsx
@@ -3,11 +3,39 @@
 import {Label} from "../../../../components/ui/label";
 import {Input} from "../../../../components/ui/input";
 import {Button} from "../../../../components/ui/button";
+import { useState } from "react";
 
 export default function RegisterForm() {
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [error, setError] = useState({
+        firstName: "",
+        lastName: "",
+        email: ""
+    });
 
 
-    const handleChange = () => {}
+    const isValidName = (value) => {
+        if (/\d/.test(value)) return "Numbers are not allowed";
+        if(!value) return "Field is Required";
+        if(value.length < 4) return "the Content must be at least 4 characters";
+        return "";
+    }
+
+    const validateEmail = (value) => {
+
+        const EmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!value) {
+            return "Email Field is Required";
+        }
+
+        if(!EmailRegex.test(value)) {
+            return "Please Enter a Valid Email"
+        }
+
+        return "";
+    }
 
 
     const handleSubmit = (e) => {
@@ -24,24 +52,51 @@ export default function RegisterForm() {
                 <div className="grid grid-cols-2 gap-4">
                     <div className="space-y-2">
                         <Label htmlFor="firstName">First name</Label>
-                        <Input id="firstName" name="firstName" onChange={handleChange}
+                        <Input id="firstName" name="firstName" onChange={(e) => {
+                            setFirstName(e.target.value);
+                            setError(prevState => ({
+                                ...prevState,
+                                firstName: isValidName(e.target.value)
+                            }))
+                        }}
                                required/>
+                        {error.firstName && (
+                            <p className="text-red-500 text-sm mt-1">{error.firstName}</p>
+                        )}
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="lastName">Last name</Label>
-                        <Input id="lastName" name="lastName" onChange={handleChange}
+                        <Input id="lastName" name="lastName" onChange={(e) => {
+                            setLastName(e.target.value);
+                            setError(prevState => ({
+                                ...prevState,
+                                lastName: isValidName(value)
+                            }));
+                        }}
                                required/>
+                        {error.lastName && (
+                            <p className="text-red-500 text-sm mt-1">{error.lastName}</p>
+                        )}
                     </div>
                 </div>
                 <div className="space-y-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" name="email"  onChange={handleChange}
+                    <Input id="email" type="email" name="email"  onChange={(e) => {
+                        setEmail(e.target.value);
+                        setError(prevState => ({
+                            ...prevState,
+                            email: validateEmail(e.target.value)
+                        }));
+                    }}
                            required/>
+                    {error.email && (
+                        <p className="text-red-500 text-sm mt-1">{error.email}</p>
+                    )}
                 </div>
                 <div className="space-y-2">
                     <Label htmlFor="password">Password</Label>
                     <Input id="password" type="password" name="password"
-                           onChange={handleChange} required/>
+                            required/>
                 </div>
                 <div className="space-y-2">
                     <Label htmlFor="confirmPassword">Password confirmation</Label>
@@ -49,7 +104,6 @@ export default function RegisterForm() {
                         id="confirmPassword"
                         type="password"
                         name="confirmPassword"
-                        onChange={handleChange}
                         required
                     />
                 </div>

6d0081a15bbff453cdef230a0cde2622a4eaeb4e
Coderyounes <younesanas2021@gmail.com>
2025-04-09 17:21:49 +0100

    Add HeroSection component to the homepage

---
+++ frontend/src/app/_home/HeroSection.jsx
@@ -0,0 +1,17 @@
+export default function HeroSection() {
+    return (
+      <>
+          <div className="py-8 flex justify-center">
+              <span className="border-2 rounded-2xl p-2 px-6 sm:text-xl">
+                  <strong>100+ Company</strong> using SynergyNow</span>
+          </div>
+
+          <div className="py-8 flex justify-center">
+            <strong className="text-center text-2xl sm:text-4xl  sm:px-56 ">
+                Empower Your business with our Seamless CRM Solutions
+            </strong>
+          </div>
+
+      </>
+    );
+}

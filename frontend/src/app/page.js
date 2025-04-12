77613c33e92239c7d3fff5c29d35bf2ad60a29ad
Coderyounes <younesanas2021@gmail.com>
2025-04-12 21:13:47 +0100

    Add TrustedSection component to Home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Layout from "./(public)/layout";
 import HeroSection from "./_home/HeroSection";
+import TrustedSection from "./_home/TrustedSection";
 export default function Home() {
   return (
       <Layout>
           <HeroSection />
+          <TrustedSection />
       </Layout>
   );
 }

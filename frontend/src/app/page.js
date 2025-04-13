5e19cb2b151a48b25fc8f5012a7477b24067ed51
Coderyounes <younesanas2021@gmail.com>
2025-04-13 03:20:46 +0100

    Add CTASection component to homepage layout

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import Layout from "./(public)/layout";
 import HeroSection from "./_home/HeroSection";
 import TrustedSection from "./_home/TrustedSection";
+import CTASection from "./_home/CTASection";
+
+
 export default function Home() {
   return (
       <Layout>
           <HeroSection />
           <TrustedSection />
+          <CTASection />
       </Layout>
   );
 }

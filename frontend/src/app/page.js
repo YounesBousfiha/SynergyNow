737d19fbace22ba21df14fcf1faa470543671ed4
Coderyounes <younesanas2021@gmail.com>
2025-04-13 15:50:38 +0100

    Add BlogSection component to the Home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,6 +3,7 @@ import Layout from "./(public)/layout";
 import HeroSection from "./_home/HeroSection";
 import TrustedSection from "./_home/TrustedSection";
 import CTASection from "./_home/CTASection";
+import BlogSection from "./_home/BlogsSection";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
           <HeroSection />
           <TrustedSection />
           <CTASection />
+          <BlogSection />
       </Layout>
   );
 }

197c698b989940d90c4ae3e7776cf2f746e8f5b9
Coderyounes <younesanas2021@gmail.com>
2025-04-16 11:09:20 +0100

    Add toaster notifications to dashboard layout

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -1,8 +1,10 @@
 import SideBar from "./_components/SideBar";
+import { Toaster } from "sonner";
 
 export default function Layout({ children }) {
   return (
       <div className="flex min-h-screen bg-[#f3f3f6]">
+          <Toaster />
         <SideBar />
         {children}
       </div>

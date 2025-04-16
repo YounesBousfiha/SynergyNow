c241394dbf1bcf34bfb9be392e1c8c1513fc0b2b
Coderyounes <younesanas2021@gmail.com>
2025-04-16 11:08:59 +0100

    Add Toaster component to public layout

--- frontend/src/app/(public)/layout.jsx
+++ frontend/src/app/(public)/layout.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import {Toaster} from "sonner";
 
 export default function Layout({ children }) {
     return (
         <>
+            <Toaster expand={true}  />
             <Header />
             { children }
             <Footer />

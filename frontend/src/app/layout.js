7be1484c47fbc2fa4ff61c56a58e02d4f631b36d
Coderyounes <younesanas2021@gmail.com>
2025-04-18 00:11:30 +0100

    Add Toaster component to layout for notifications
    Integrates the Toaster component from the "sonner" library into the root layout to enable user-friendly notifications. This change sets up the groundwork for displaying toast messages across the application.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./globals.css";
+import {Toaster} from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>

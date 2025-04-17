95249eecbe5373dafa723c2a1f41d8d2f1fa24c5
Coderyounes <younesanas2021@gmail.com>
2025-04-17 14:39:24 +0100

    Remove unused imports from dashboard layout component
    Cleaned up unused imports, including 'useEffect', 'useState', and 'useRouter', to improve code readability and maintainability. This helps reduce potential confusion and ensures the file only contains necessary dependencies.

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -1,8 +1,5 @@
-"use client"
 import SideBar from "./_components/SideBar";
 import { Toaster } from "sonner";
-import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation'
 
 export default function Layout({ children }) {
 

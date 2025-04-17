fbb1764e875e9eb1c9ff215019fa1a7012b86b45
Coderyounes <younesanas2021@gmail.com>
2025-04-17 01:26:43 +0100

    Add redirection to /info-setup for empty company info
    Introduce a check for the '/dashboard' route in middleware to redirect users to '/info-setup' if their company information is missing. Also, handle potential errors during the API call to fetch company details.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,9 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
 import { PROTECTED_ROUTE, AUTH_ROUTE } from './constants/config';
+import { myCompanyService} from "./services/myCompanyServices";
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
     const token = request.cookies.get('jwt');
-
 
     const protectedRoutes = Object.values(PROTECTED_ROUTE).some((route) => (
         request.nextUrl.pathname.startsWith(route)
@@ -11,6 +11,21 @@ export default function middleware(request: NextRequest) {
 
     if(protectedRoutes && !token) {
         return NextResponse.redirect(new URL(AUTH_ROUTE.LOGIN, request.url));
+    }
+
+    if(request.nextUrl.pathname.startsWith('/dashboard')) {
+        try {
+             const res = await myCompanyService.getCompanyInfo(token.value);
+             console.log(res);
+             console.log(res.data.message.length);
+             if (res.data.message.length === 0) {
+                 console.log("hey");
+                 return NextResponse.redirect(new URL('/info-setup', request.url));
+             }
+         } catch (error) {
+            console.error("Error in middleware:", error);
+        }
+
     }
 
     if((request.nextUrl.pathname === AUTH_ROUTE.LOGIN || request.nextUrl.pathname === AUTH_ROUTE.REGISTER) && token) {

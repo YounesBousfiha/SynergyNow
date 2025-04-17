b2c77c927f065d37a9f4af4888dd420003b1ea4f
Coderyounes <younesanas2021@gmail.com>
2025-04-18 00:11:59 +0100

    Update company retrieval logic in MyCompanyController
    Revised the company fetching mechanism to use the `employes_at` field from the associated user instead of the owner's ID. This ensures accurate company data retrieval based on user employment.

---
+++ frontend/src/app/invitation/page.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {  useSearchParams } from  'next/navigation';
+import {useState} from "react";
+import RegisterByInvitation from './_componenets/RegisterByInvitation'
+
+
+export default function RegisterByInvitationPage() {
+
+    const seachParam = useSearchParams();
+
+    const base64data = seachParam.get('invitation_token');
+
+    const [companyName, invitation_token] = atob(base64data).split(':');
+
+    return (
+        <>
+            <main className="min-h-screen flex items-center justify-center bg-gray-50">
+                <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg mx-4 my-8">
+                    <div className="text-center">
+                        <h1 className="text-2xl font-bold text-gray-900 mb-2">Register to { companyName }</h1>
+                        <p className="text-gray-600 mb-6">Complete your registration</p>
+                    </div>
+                    <RegisterByInvitation token={invitation_token} />
+                </div>
+            </main>
+        </>
+    );
+}

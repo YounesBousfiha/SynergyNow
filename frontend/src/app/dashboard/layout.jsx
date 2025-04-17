220535c71fe67f9b94726d96e933ab5c96f3f56b
Coderyounes <younesanas2021@gmail.com>
2025-04-17 01:26:34 +0100

    Add "use client" directive to dashboard layout component
    This update ensures the layout component is explicitly marked as a client component, aligning with Next.js requirements. Adjustments also include importing necessary hooks for potential client-side interactions.

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -1,12 +1,16 @@
+"use client"
 import SideBar from "./_components/SideBar";
 import { Toaster } from "sonner";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation'
 
 export default function Layout({ children }) {
-  return (
-      <div className="flex min-h-screen bg-[#f3f3f6]">
-          <Toaster />
-        <SideBar />
-        {children}
-      </div>
-  );
+
+    return (
+        <div className="flex min-h-screen bg-[#f3f3f6]">
+            <Toaster />
+            <SideBar />
+            {children}
+        </div>
+    );
 }

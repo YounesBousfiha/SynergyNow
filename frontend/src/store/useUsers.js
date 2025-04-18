3dfa5b3f96c81b4e9dc42bffb57304ea71cb8e7d
Coderyounes <younesanas2021@gmail.com>
2025-04-18 17:12:29 +0100

    Add Zustand store for managing users
    Introduces a new Zustand store (`useUsersStore`) to manage user-related state. Includes functions to add users (`setUsers`) and remove users by ID (`removeUsers`). This sets up the foundation for handling user state in the frontend.

---
+++ frontend/src/store/useUsers.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+export const useUsersStore = create(
+    (set) => ( {
+
+        users: [],
+        setUsers: (users) => set((state) => ({
+            users: [...state.users, ...users]
+                })),
+        removeUsers: (id) => set((state) => ({
+            users: state.users.filter((user) => user.id !== id)
+        })),
+    })
+)

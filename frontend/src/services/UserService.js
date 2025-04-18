fc3888aedb3fceeedbc3fcc0db0f69406e14e779
Coderyounes <younesanas2021@gmail.com>
2025-04-18 17:12:35 +0100

    Add UserService with user management methods
    Introduced a new UserService module to handle user-related API operations. It includes methods for fetching all users, retrieving user details, changing user roles, and deleting users, utilizing JWT for authorization.

---
+++ frontend/src/services/UserService.js
@@ -0,0 +1,49 @@
+import axios from '../lib/axios';
+import Cookies from "js-cookie";
+
+export const userService = {
+    getAllUsers: async () => {
+        try {
+            return await axios.get('/users', {
+                headers: {
+                    'Authorization' : `Bearer ${Cookies.get('jwt')}`
+                }
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getUserDetails: async (userId) => {
+        try {
+            return await axios.get(`/users/${userId}`, {
+                headers: {
+                    'Authorization' : `Bearer ${Cookies.get('jwt')}`
+                }
+            });
+        } catch (error) {
+            throw new Error(error)
+        }
+    },
+    changeUserRole: async (userId) => {
+        try {
+            return await axios.patch(`/users/${userId}`, {
+                headers: {
+                    'Authorization' : `Bearer ${Cookies.get('jwt')}`
+                }
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    deleteUser: async (userId) => {
+        try {
+            return await axios.delete(`/user/${userId}`, {
+                headers: {
+                    'Authorization' : `Bearer ${Cookies.get('jwt')}`
+                }
+            })
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}

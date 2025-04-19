6afbe2c6df0aeabd40774b364a85847f4cb348da
Coderyounes <younesanas2021@gmail.com>
2025-04-19 14:44:49 +0100

    Refactor dashboard layout to include DashBar and main wrapper.
    Introduced a `DashBar` component above the main content and wrapped children within a styled `main` wrapper for better structure. This improves layout clarity and maintains consistency across the dashboard interface.

--- frontend/src/app/dashboard/layout.jsx
+++ frontend/src/app/dashboard/layout.jsx
@@ -1,13 +1,17 @@
 import SideBar from "./_components/SideBar";
 import { Toaster } from "sonner";
-
+import DashBar from './_components/DashBoardBar';
 export default function Layout({ children }) {
-
     return (
         <div className="flex min-h-screen bg-[#f3f3f6]">
-            <Toaster richColors={true}/>
+            <Toaster richColors={true} />
             <SideBar />
-            {children}
+            <div className="flex flex-col flex-1">
+                <DashBar />
+                <main className="flex-1 p-4">
+                    {children}
+                </main>
+            </div>
         </div>
     );
 }

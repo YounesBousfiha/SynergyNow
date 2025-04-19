f9dd066fbfbe0e8c592d8b0d0b7ab2a143873fe1
Coderyounes <younesanas2021@gmail.com>
2025-04-19 11:25:59 +0100

    Add user details view and edit functionality in UserRow
    Introduced a `UserViewSheet` component for viewing and editing user details, including profile image, name, email, and role. Updated `UserRow` with a view button and conditional rendering for delete functionality based on user roles. Added helper function `SplitFullName` to split full names into first and last names.

--- frontend/src/app/dashboard/administration/settings/_componenets/UserRow.jsx
+++ frontend/src/app/dashboard/administration/settings/_componenets/UserRow.jsx
@@ -1,19 +1,28 @@
 import {Button} from "../../../../../components/ui/button";
+import Image from "next/image";
+import { Card, CardHeader, CardTitle, CardContent } from '../../../../../components/ui/card';
+import { Label } from '../../../../../components/ui/label';
+import { Input} from "../../../../../components/ui/input";
+import { Select, SelectTrigger, SelectItem, SelectContent, SelectValue } from "../../../../../components/ui/select";
 import { Badge } from "../../../../../components/ui/badge";
-import { Trash2 } from 'lucide-react';
+import { Trash2, Eye, Pencil } from 'lucide-react';
 import {useState} from "react";
 import { useUsersStore } from '../../../../../store/useUsers';
 import {toast} from "sonner";
 import {userService} from "../../../../../services/UserService";
+import { useAuth } from '../../../../../store/useAuth';
+import { Sheet, SheetContent, SheetTrigger, SheetTitle} from '../../../../../components/ui/sheet';
 export default function UserRow({
-    id,
+                                            id,
                                             name,
+                                            image,
                                             email,
                                             role,
                                             last_login_at
                                         }) {
 
     const { removeUsers } = useUsersStore();
+    const { user } = useAuth();
     const [isOpen, setIsOpen] = useState(false);
 
     const getRoleBadge = (role) => {
@@ -41,6 +50,8 @@ export default function UserRow({
         }
     }
 
+    const handleEdit = async () => {}
+
     return (
         <tr className="border-b hover:bg-gray-50">
             <td className="py-4 px-6">{name}</td>
@@ -49,11 +60,142 @@ export default function UserRow({
             <td className="py-4 px-6">{last_login_at}</td>
             <td className="py-4 px-6">
                 <div className="flex items-center gap-2">
-                    <Button onClick={handleDelete} variant="ghost" size="icon" className="h-8 w-8 text-red-500">
-                        <Trash2 size={18}/>
+                    <UserViewSheet
+                        user={{
+                            id,
+                            name,
+                            email,
+                            role,
+                            last_login_at,
+                            image
+                        }}
+                    />
+                    <Button onClick={handleEdit} variant="ghost" size="icon" className="h-8 w-8 text-green-500 hover:bg-green-500 hover:text-white">
+                        <Pencil size={18}/>
                     </Button>
+                    { user.role_id !== 1 ? (
+                        <Button onClick={handleDelete} variant="ghost" size="icon" className="h-8 w-8 text-red-500 hover:bg-red-500 hover:text-white">
+                            <Trash2 size={18}/>
+                        </Button>
+                    ) : null }
                 </div>
             </td>
         </tr>
     )
+}
+
+function UserViewSheet({ user }) {
+    const handleSubmit = () => {}
+
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="h-8 w-8 text-blue-500 hover:bg-blue-500 hover:text-white">
+                    <Eye size={18} />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="right" className="overflow-y-auto">
+                <SheetTitle className="text-lg font-semibold text-center">User Details</SheetTitle>
+
+                <div className="p-4 flex justify-center">
+                    <div className="rounded-full flex justify-center overflow-hidden w-44 h-44">
+                        <Image
+                            src={user.image || "https://res.cloudinary.com/dashccxm0/image/upload/v1745004599/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp_ire0uq.jpg"}
+                            alt="User Profile"
+                            className="object-cover"
+                            width={180}
+                            height={180}
+                        />
+                    </div>
+                </div>
+
+                <div className="space-y-6">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Current Details</CardTitle>
+                        </CardHeader>
+                        <CardContent className="space-y-3">
+                            <div className="flex items-center text-gray-700">
+                                <span className="font-medium w-24">FirstName:</span>
+                                <span>{SplitFullName(user.name).firstName}</span>
+                            </div>
+                            <div className="flex items-center text-gray-700">
+                                <span className="font-medium w-24">Lastname:</span>
+                                <span>{SplitFullName(user.name).lastName}</span>
+                            </div>
+                            <div className="flex items-center text-gray-700">
+                                <span className="font-medium w-24">Email:</span>
+                                <span>{user.email}</span>
+                            </div>
+                            <div className="flex items-center text-gray-700">
+                                <span className="font-medium w-24">Role:</span>
+                                <span>{user.role}</span>
+                            </div>
+                        </CardContent>
+                    </Card>
+
+                    {/* Edit Form Card */}
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Edit Details</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <form onSubmit={handleSubmit} className="space-y-4">
+                                <div className="space-y-2">
+                                    <Label htmlFor="firstname">Name</Label>
+                                    <Input
+                                        id="firstname"
+                                        defaultValue={SplitFullName(user.name).firstName}
+                                        type="text"
+                                    />
+                                </div>
+                                <div className="space-y-2">
+                                    <Label htmlFor="lastname">Name</Label>
+                                    <Input
+                                        id="lastname"
+                                        defaultValue={SplitFullName(user.name).lastName}
+                                        type="text"
+                                    />
+                                </div>
+
+                                <div className="space-y-2">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        defaultValue={user.email}
+                                        type="email"
+                                    />
+                                </div>
+
+                                <div className="space-y-2">
+                                    <Label htmlFor="role">Role</Label>
+                                    <Select defaultValue={user.role}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select a role"/>
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="2">Admin</SelectItem>
+                                            <SelectItem value="3">Agent</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+
+                                <Button
+                                    type="submit"
+                                    className="text-white bg-[#06ae6f] hover:bg-[#296c5c] hover:text-white w-full"
+                                >
+                                    Save Changes
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+function SplitFullName(name) {
+    const [firstName, lastName] = name.split(' ');
+    return {firstName, lastName};
 }

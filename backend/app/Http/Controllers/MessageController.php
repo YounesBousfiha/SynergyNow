e4a33d082644055bf6e286ebea1e11fc01c52459
Coderyounes <younesanas2021@gmail.com>
2025-04-20 14:19:09 +0100

    Add GetContacts method and fix sender ID retrieval
    Introduced a new GetContacts method in MessageController to fetch users associated with a company. Also corrected the sender ID retrieval in sendMessage to use the proper helper method.

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -6,14 +6,35 @@ use App\Events\MessageSent;
 use App\Http\Helpers\AuthHelpers;
 use App\Models\Chat;
 use App\Models\Message;
+use App\Models\MyCompany;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Event;
 
 class MessageController extends Controller
 {
+
+    public function GetContacts(Request $request) {
+
+        try {
+            $user_id = AuthHelpers::getId($request->bearerToken());
+            $company = MyCompany::where('employes_at', $user_id)->first();
+            //$company = MyCompany::where('owner_id', $owner_id)->first();
+            $users = User::where('employes_at', $company['id'])->get();
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Unexpected Error'
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => $users
+        ]);
+    }
     public  function sendMessage(Request $request, string $chatId) {
         //$data = $request->validate();
         $data = request()->all();
+        //dump($data);
         try {
             $chat = Chat::find($chatId);
             if(!$chat) {
@@ -21,7 +42,7 @@ class MessageController extends Controller
                     'message' => 'chat not found'
                 ], 404);
             }
-            $sender_id = AuthHelpers::getMyCompany($request->bearerToken())->id;
+            $sender_id = AuthHelpers::getId($request->bearerToken());
             $data['chat_id'] = $chatId;
             $data['sender_id'] = $sender_id;
             $message = Message::create($data);

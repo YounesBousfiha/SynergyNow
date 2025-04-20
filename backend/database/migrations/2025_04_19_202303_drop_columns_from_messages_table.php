6b8bfee8d7a0c9edcf3cccc085121267b97287ca
Coderyounes <younesanas2021@gmail.com>
2025-04-20 02:55:56 +0100

    Add and remove sender_id and receiver_id in messages table
    Added a migration to include a nullable sender_id column in the messages table with a foreign key constraint. Created another migration to drop sender_id and receiver_id columns, including their foreign key constraints, for rollback purposes.

---
+++ backend/database/migrations/2025_04_19_202303_drop_columns_from_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['sender_id']);
+            $table->dropForeign(['receiver_id']);
+
+            $table->dropColumn(['sender_id', 'receiver_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            //
+        });
+    }
+};

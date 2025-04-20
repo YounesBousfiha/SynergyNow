acc62bbba8d21808ee910ed393f37ae20d64eb92
Coderyounes <younesanas2021@gmail.com>
2025-04-20 14:19:18 +0100

    Add messageService with sendMessage function
    Introduce a new service for handling message-related API calls. The sendMessage function allows sending messages to a specific chat using an authenticated request with a JWT token.

---
+++ frontend/src/services/messageService.js
@@ -0,0 +1,16 @@
+import axios from '../lib/axios';
+import Cookies from "js-cookie";
+
+export const messageService = {
+    sendMessage: async (data, chatId) => {
+        try {
+            return await axios.post(`/messages/${chatId}`, data, {
+                headers: {
+                    'Authorization' : `Bearer ${Cookies.get('jwt')}`
+                }
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}

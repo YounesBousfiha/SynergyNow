2342875c259349fba8ac717cc7981d1450380ae4
Coderyounes <younesanas2021@gmail.com>
2025-04-21 20:46:38 +0100

    Add Cloudinary singleton binding in AppServiceProvider
    This update registers Cloudinary as a singleton service in the application container. It utilizes environment variables for configuration to ensure secure and dynamic setup. This change enables seamless integration of Cloudinary services.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Cloudinary\Cloudinary;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(Cloudinary::class, function() {
+            return new Cloudinary([
+               'cloud' => [
+                   'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                   'api_key' => env('CLOUDINARY_API_KEY'),
+                   'api_secret' => env('CLOUDINARY_API_SECRET')
+               ],
+               'url' => [
+                   'secure' => true
+               ]
+            ]);
+        });
     }
 
     /**

a61b02f4319c0a56a2ca7691a4928d43c1eace23
Coderyounes <younesanas2021@gmail.com>
2025-04-21 21:00:04 +0100

    Add form state handling and enhance company creation logic
    Integrated controlled inputs for form fields to manage state effectively, ensuring real-time updates and better UX. Adjusted client creation logic to append all fields and handle logo uploads as base64. Refined layout to maintain functionality and consistency.

--- frontend/src/app/dashboard/companies/_componenets/AddCompanyBtn.jsx
+++ frontend/src/app/dashboard/companies/_componenets/AddCompanyBtn.jsx
@@ -22,6 +22,14 @@ import { useCompanyStore } from "../../../../store/useCompany"
 
 export default function AddCompanyBtn() {
     const [imagePreview, setImagePreview] = useState(null);
+    const [name, setName ] = useState("");
+    const [description, setDescription] = useState("");
+    const [industry, setIndustry] = useState("");
+    const [address, setAddress] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [website, setWebsite] = useState("");
+
     const {  setClients, addClient} = useCompanyStore();
 
 
@@ -29,6 +37,7 @@ export default function AddCompanyBtn() {
         async function fetchData () {
             try {
                 const response  = await companyService.all();
+                console.log(response.data.clientCompanies);
                 setClients(response.data.clientCompanies);
             } catch (e) {
                 toast.error(e.message())
@@ -52,16 +61,26 @@ export default function AddCompanyBtn() {
         e.preventDefault();
 
         const formData = new FormData(e.target);
-        const file = e.target.querySelector('#logo-upload').files[0];
+        const imageFormat = imagePreview.split(";")[0].split('/')[1];
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('industry', industry);
+        formData.append('address', address);
+        formData.append('email', email);
+        formData.append('phone', phone);
+        formData.append('website', website);
+        if(imagePreview) {
+            const base64response = await fetch(imagePreview);
+            const blob = await base64response.blob();
 
-        const rawData = Object.fromEntries(formData);
-        const data = {
-            ...rawData,
-            image: file
+            const file = new File([blob], 'company-logo.png', {
+                type: `image/${imageFormat}`
+            })
+            formData.append('image', file);
         }
 
         try {
-            const response = await companyService.create(data);
+            const response = await companyService.create(formData);
             console.log(response.data.message);
             addClient(response.data.message);
             toast.success("Client Added Successfully");
@@ -88,84 +107,135 @@ export default function AddCompanyBtn() {
                                 </SheetDescription>
                             </SheetHeader>
                             <form onSubmit={handleSubmit}>
-                            <div className="grid gap-4 py-4 px-4">
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="name" className="text-right">
-                                        Name
-                                    </Label>
-                                    <Input id="name" name="name" className="col-span-3"/>
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="description" name="description" className="text-right">
-                                        Description
-                                    </Label>
-                                    <Textarea className="col-span-3" id="description" name="description" placeholder="Type your message here." />                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="industry" className="text-right">
-                                        Industry
-                                    </Label>
-                                    <Input id="industry" name="industry" className="col-span-3"/>
+                                <div className="grid gap-4 py-4 px-4">
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="name" className="text-right">
+                                            Name
+                                        </Label>
+                                        <Input
+                                            id="name"
+                                            name="name"
+                                            value={name}
+                                            className="col-span-3"
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="description" name="description" className="text-right">
+                                            Description
+                                        </Label>
+                                        <Textarea
+                                            className="col-span-3"
+                                            id="description"
+                                            name="description"
+                                            value={description}
+                                            placeholder="Type your message here."
+                                            onChange={(e) => setDescription(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="industry" className="text-right">
+                                            Industry
+                                        </Label>
+                                        <Input
+                                            id="industry"
+                                            name="industry"
+                                            className="col-span-3"
+                                            value={industry}
+                                            onChange={(e) => setIndustry(e.target.value)}
+                                        />
 
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="address" className="text-right">
-                                        Address
-                                    </Label>
-                                    <Input id="address" name="address" className="col-span-3"/>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="address" className="text-right">
+                                            Address
+                                        </Label>
+                                        <Input
+                                            id="address"
+                                            name="address"
+                                            className="col-span-3"
+                                            value={address}
+                                            onChange={(e) => setAddress(e.target.value)}
+                                        />
 
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="email" className="text-right">
-                                        Email
-                                    </Label>
-                                    <Input id="email" name="email" className="col-span-3"/>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="email" className="text-right">
+                                            Email
+                                        </Label>
+                                        <Input
+                                            id="email"
+                                            name="email"
+                                            value={email}
+                                            className="col-span-3"
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
 
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="phone" className="text-right">
-                                        Phone
-                                    </Label>
-                                    <Input id="phone" name="phone" className="col-span-3"/>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="phone" className="text-right">
+                                            Phone
+                                        </Label>
+                                        <Input
+                                            id="phone"
+                                            name="phone"
+                                            value={phone}
+                                            className="col-span-3"
+                                            onChange={(e) => setPhone(e.target.value)}
+                                        />
 
-                                </div>
-                                <div className="grid grid-cols-4 items-center gap-4">
-                                    <Label htmlFor="logo" className="text-right">
-                                        Logo
-                                    </Label>
-                                    <div className="col-span-3">
-                                        <div className="flex items-center gap-4">
-                                            <Button
-                                                type="button"
-                                                variant="outline"
-                                                className="cursor-pointer"
-                                                onClick={() => document.getElementById('logo-upload').click()}
-                                            >
-                                                <Upload size={16} className="mr-2"/>
-                                                Upload logo
-                                            </Button>
-                                            <Input
-                                                id="logo-upload"
-                                                type="file"
-                                                accept="image/*"
-                                                onChange={handleFileChange}
-                                                className="hidden"
-                                            />
-                                        </div>
-                                        {imagePreview && (
-                                            <div className="mt-4 flex justify-center">
-                                                <div
-                                                    className="bg-gray-50 p-3 rounded-full w-32 h-32 flex items-center justify-center shadow-sm border border-gray-100">
-                                                    <img
-                                                        src={imagePreview}
-                                                        alt="Logo preview"
-                                                        className="max-w-full max-h-full object-cover rounded-full"
-                                                    />
-                                                </div>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="website" className="text-right">
+                                            Website
+                                        </Label>
+                                        <Input
+                                            id="website"
+                                            name="website"
+                                            value={website}
+                                            className="col-span-3"
+                                            onChange={(e) => setWebsite(e.target.value)}
+                                        />
+
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="logo" className="text-right">
+                                            Logo
+                                        </Label>
+                                        <div className="col-span-3">
+                                            <div className="flex items-center gap-4">
+                                                <Button
+                                                    type="button"
+                                                    variant="outline"
+                                                    className="cursor-pointer"
+                                                    onClick={() => document.getElementById('logo-upload').click()}
+                                                >
+                                                    <Upload size={16} className="mr-2"/>
+                                                    Upload logo
+                                                </Button>
+                                                <Input
+                                                    id="logo-upload"
+                                                    type="file"
+                                                    accept="image/*"
+                                                    onChange={handleFileChange}
+                                                    className="hidden"
+                                                />
                                             </div>
-                                        )}
+                                            {imagePreview && (
+                                                <div className="mt-4 flex justify-center">
+                                                    <div
+                                                        className="bg-gray-50 p-3 rounded-full w-32 h-32 flex items-center justify-center shadow-sm border border-gray-100">
+                                                        <img
+                                                            src={imagePreview}
+                                                            alt="Logo preview"
+                                                            className="max-w-full max-h-full object-cover rounded-full"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                                 <SheetFooter>
                                     <SheetClose asChild>
                                         <Button type="submit">Save changes</Button>

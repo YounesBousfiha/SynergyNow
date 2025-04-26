554891443e2eb7bb7557127b2c068c24fb8f2656
Coderyounes <younesanas2021@gmail.com>
2025-04-27 00:59:04 +0100

    Add UpdateDialog for task updates and integrate with TaskCard

--- frontend/src/app/dashboard/scrumboard/tasks/_components/TaskCard.jsx
+++ frontend/src/app/dashboard/scrumboard/tasks/_components/TaskCard.jsx
@@ -1,17 +1,22 @@
+"use client"
+
 import {Card, CardContent, CardHeader} from "../../../../../components/ui/card";
 import {Button} from "../../../../../components/ui/button";
-import {AlarmClock, MoreVertical, Eye, Trash2 } from "lucide-react";
+import {AlarmClock, MoreVertical, Eye, FilePen } from "lucide-react";
 import {Badge} from "../../../../../components/ui/badge";
 import {Avatar, AvatarFallback, AvatarImage} from "../../../../../components/ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../../../../../components/ui/dropdown-menu";
 import {taskService} from "../../../../../services/taskService";
 import { useTasksStore } from "../../../../../store/useTasks";
 import {toast} from "sonner";
-
+import { useState, useEffect} from "react";
 import DeleteDialog from "./DeleteDialog";
+import UpdateDialog from "./UpdateDialog";
 export default function TaskCard({ id, title, due_date, priority }) {
 
     const { removeTask, updateTask } = useTasksStore();
+    const [isUpdateOpen, setIsUpdateOpen] = useState(false);
+
     const handleDelete = async (id) => {
         try {
             const response = await taskService.deleteTask(id);
@@ -24,11 +29,35 @@ export default function TaskCard({ id, title, due_date, priority }) {
             toast.error("Error deleting task");
         }
     }
+
+    const handleUpdate = async (id, data) => {
+        try {
+            const response = await taskService.updateTask(id, data);
+            if(response.status === 200) {
+                toast.success("Task updated successfully");
+                updateTask(id, data);
+                setIsUpdateOpen(false);
+            }
+        } catch (error) {
+            console.error("Error updating task: ", error);
+            toast.error("Error updating task");
+        }
+    }
     const handleView  = async (id) => {
         console.log("View task with id: ", id)
 
     }
+
+
     return (
+        <>
+            <UpdateDialog
+                handleUpdate={handleUpdate}
+                id={id}
+                task={{ title, due_date, priority }}
+                open={isUpdateOpen}
+                onOpenChange={setIsUpdateOpen}
+            />
         <Card className="bg-white shadow-sm">
             <CardHeader className="p-3 pb-0 flex flex-row items-start justify-between space-y-0">
                 <h4 className="text-sm font-medium">{title}</h4>
@@ -46,11 +75,8 @@ export default function TaskCard({ id, title, due_date, priority }) {
                             <Eye size={14} />
                             <span>View</span>
                         </DropdownMenuItem>
-                        <DropdownMenuItem
-                            className="flex items-center gap-2"
-                        >
-                            <Eye size={14} />
-                            <span>update</span>
+                        <DropdownMenuItem onClick={() => setIsUpdateOpen(true)}>
+                            update
                         </DropdownMenuItem>
                         <DropdownMenuItem
                             className="flex items-center gap-2 text-red-500 hover:!text-red-600 hover:!bg-red-100 hover:cursor-pointer"
@@ -60,6 +86,7 @@ export default function TaskCard({ id, title, due_date, priority }) {
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
+
             </CardHeader>
             <CardContent className="p-3 pt-2">
                 <div className="flex items-center justify-between">
@@ -84,5 +111,6 @@ export default function TaskCard({ id, title, due_date, priority }) {
                 </div>
             </CardContent>
         </Card>
+    </>
     )
 }

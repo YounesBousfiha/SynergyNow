e11b70dd5ce4b1aadda8353fd10c06604b469383
Coderyounes <younesanas2021@gmail.com>
2025-04-27 14:31:02 +0100

    Add dynamic deal fetching and loading state to Scrumboard
    Implemented fetching of deals data from the backend and integrated it into the dashboard. Added state management and loading spinner to improve user experience. Replaced static deal cards with dynamically rendered cards based on deal status.

--- frontend/src/app/dashboard/scrumboard/deals/page.jsx
+++ frontend/src/app/dashboard/scrumboard/deals/page.jsx
@@ -1,7 +1,55 @@
+"use client"
+
 import DealsColumn from "./_components/DealsColumn";
 import DealCard from "./_components/DealCard";
+import { useState, useEffect} from "react";
+import {toast} from "sonner";
+import { dealsService} from "../../../../services/dealsService";
 
 export default function DealsManagerPage() {
+
+    const [deals, setDeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        async function fetchDeals() {
+            try {
+                const response = await dealsService.getDeals();
+                console.log("Deals:", response);
+                setDeals(response.message);
+                setIsLoading(false);
+
+            } catch (error) {
+                toast.error("Error while loading the deals")
+            }
+        }
+
+        fetchDeals()
+    }, [])
+
+
+    const newDeals = deals.filter(deal => deal.status === "new");
+    const followUpsDeals = deals.filter(deal => deal.status === "follow_up");
+    const underReviewDeals = deals.filter(deal => deal.status === "under_review");
+    const wonDeals = deals.filter(deal => deal.status === "closed_won");
+    const lostDeals = deals.filter(deal => deal.status === "closed_not_won");
+
+
+    const newCount = newDeals.length;
+    const followUpsCount = followUpsDeals.length;
+    const underReviewCount = underReviewDeals.length;
+    const wonCount = wonDeals.length;
+    const lostCount = lostDeals.length;
+
+    if(isLoading) {
+        return (
+            <div className="p-8 flex items-center justify-center min-h-screen">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#296c5c]"></div>
+            </div>
+        )
+    }
+
     return (
             <div className="flex-1">
                 {/* Content */}
@@ -9,34 +57,48 @@ export default function DealsManagerPage() {
                     <h1 className="text-2xl font-bold mb-6">Deals Manager</h1>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 min-w-max">
-                        {/* Unassigned Column */}
-                        <DealsColumn title="Unassigned" count={1}>
-                            <DealCard />
-                        </DealsColumn>
 
                         {/* New Column */}
-                        <DealsColumn title="New" count={2}>
-                            <DealCard />
-                            <DealCard />
+                        <DealsColumn title="New" count={newCount}>
+                            {newDeals.map((deal) => {
+                                return (
+                                    <DealCard key={deal.id} id={deal.id} deal={deal}/>
+                                )
+                            })}
                         </DealsColumn>
 
                         {/* FollowUps Column */}
-                        <DealsColumn title="FollowUps" count={1}>
-                            <DealCard />
+                        <DealsColumn title="FollowUps" count={followUpsCount}>
+                            {followUpsDeals.map((deal) => {
+                                return (
+                                    <DealCard key={deal.id} id={deal.id} deal={deal}/>
+                                )
+                            })}
                         </DealsColumn>
 
                         {/* UnderReview Column */}
-                        <DealsColumn title="UnderReview" count={2}>
-                            <DealCard />
-                            <DealCard />
+                        <DealsColumn title="UnderReview" count={underReviewCount}>
+                            {underReviewDeals.map((deal) => {
+                                return (
+                                    <DealCard key={deal.id} id={deal.id} deal={deal}/>
+                                )
+                            })}
                         </DealsColumn>
 
                         {/* Demo Column */}
-                        <DealsColumn title="Won" count={1}>
-                            <DealCard />
+                        <DealsColumn title="Won" count={wonCount}>
+                            {wonDeals.map((deal) => {
+                                return (
+                                    <DealCard key={deal.id} id={deal.id} deal={deal}/>
+                                )
+                            })}
                         </DealsColumn>
-                        <DealsColumn title="Lost" count={1}>
-                            <DealCard />
+                        <DealsColumn title="Lost" count={lostCount}>
+                            {lostDeals.map((deal) => {
+                                return (
+                                    <DealCard key={deal.id} id={deal.id} deal={deal}/>
+                                )
+                            })}
                         </DealsColumn>
                     </div>
                 </main>

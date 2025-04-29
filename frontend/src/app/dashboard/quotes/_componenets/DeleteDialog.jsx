91a921c1118ffdb747e146c15fa33dffb25ce5a6
Coderyounes <younesanas2021@gmail.com>
2025-04-29 03:10:55 +0100

    Add DeleteDialog component for confirming deletion actions
    The DeleteDialog component provides a confirmation dialog with clear warnings before performing irreversible delete actions. It uses the AlertDialog UI pattern and integrates seamlessly with existing components. This enhances user experience by preventing accidental deletions.

---
+++ frontend/src/app/dashboard/quotes/_componenets/DeleteDialog.jsx
@@ -0,0 +1,33 @@
+import {
+    AlertDialog, AlertDialogAction, AlertDialogCancel,
+    AlertDialogContent, AlertDialogDescription, AlertDialogFooter,
+    AlertDialogHeader, AlertDialogTitle,
+    AlertDialogTrigger
+} from "../../../../components/ui/alert-dialog";
+import {Trash2} from "lucide-react";
+
+export default function DeleteDialog({ handleDelete, id}) {
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <div className="flex gap-2 text-red-500">
+                    <Trash2 color={"red"} size={18} />
+                    Delete
+                </div>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction className="bg-red-500 hover:bg-red-500" onClick={() => handleDelete(id)}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}

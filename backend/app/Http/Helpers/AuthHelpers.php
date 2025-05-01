c03dfd15363d796ee8477d2247a645dcf25bb912
Coderyounes <younesanas2021@gmail.com>
2025-05-01 21:52:21 +0100

    Add methods to retrieve user role and company employees from JWT token

--- backend/app/Http/Helpers/AuthHelpers.php
+++ backend/app/Http/Helpers/AuthHelpers.php
@@ -3,6 +3,7 @@
 namespace App\Http\Helpers;
 
 use App\Models\MyCompany;
+use App\Models\User;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
@@ -19,7 +20,18 @@ class AuthHelpers
     public  static function  getMyCompany(string $token) {
         $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
         $userId = $decoded->sub;
-        return MyCompany::where('owner_id', $userId)->first(); // TODO: update to handle both Owner & Employes using orWhere
+        return MyCompany::where('owner_id', $userId)->first();
     }
 
+    public static function getCompanyEmployesAt(string $token) {
+        $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $userID = $decoded->sub;
+        return User::where('id', $userID)->first()->employes_at;
+    }
+
+    public static function getUserRole(string $token) {
+        $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+
+        return $decoded->role;
+    }
 }

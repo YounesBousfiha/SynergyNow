ad30d5f69d14f6dcb5156660f6a294777f48a463
Coderyounes <younesanas2021@gmail.com>
2025-05-03 03:59:00 +0100

    Restrict add card button visibility based on user role

--- frontend/src/app/dashboard/scrumboard/tasks/_components/KanbanColumn.jsx
+++ frontend/src/app/dashboard/scrumboard/tasks/_components/KanbanColumn.jsx
@@ -5,9 +5,12 @@ import {Button} from "../../../../../components/ui/button";
 import {Plus} from "lucide-react";
 import {useState} from 'react';
 import AddCardSheet from "./AddCardSheet";
+import {useAuth} from "../../../../../store/useAuth";
 export default function KanbanColumn({ title, count, children }) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const roleId = useAuth((state) => state.user?.role_id);
+
     const onAddCard = () => {
         setIsOpen(true);
     }
@@ -21,14 +24,16 @@ export default function KanbanColumn({ title, count, children }) {
                         {count}
                     </Badge>
                 </div>
-                <Button
+
+                { roleId !== 3 &&
+                    <Button
                     variant="ghost"
                     size="icon"
                     className="h-8 w-8 rounded-full bg-gray-100"
                     onClick={() => onAddCard()}
                 >
                     <Plus size={16} />
-                </Button>
+                </Button>}
             </div>
             <div className="space-y-4">{children}</div>
 

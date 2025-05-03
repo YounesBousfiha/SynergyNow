1ce6e94c78affb23da3d37296bfafad100c2f734
Coderyounes <younesanas2021@gmail.com>
2025-05-03 03:58:49 +0100

    Restrict add deal button visibility based on user role

--- frontend/src/app/dashboard/scrumboard/deals/_components/DealsColumn.jsx
+++ frontend/src/app/dashboard/scrumboard/deals/_components/DealsColumn.jsx
@@ -5,10 +5,13 @@ import {Button} from "../../../../../components/ui/button";
 import {Plus} from "lucide-react";
 import AddDealSheet from "./AddDealSheet";
 import { useState } from 'react';
+import {useAuth} from "../../../../../store/useAuth";
 
 export default function DealsColumn({ title, count, children }) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const roleId = useAuth((state) => state.user?.role_id);
+
     const onAddDeal = () => {
         setIsOpen(true)
     }
@@ -22,14 +25,14 @@ export default function DealsColumn({ title, count, children }) {
                         {count}
                     </Badge>
                 </div>
-                <Button
+                { roleId !== 3 && <Button
                     variant="ghost"
                     size="icon"
                     className="h-8 w-8 rounded-full bg-gray-100"
                     onClick={() => onAddDeal()}
                 >
-                    <Plus size={16} />
-                </Button>
+                    <Plus size={16}/>
+                </Button>}
             </div>
             <div className="space-y-4">{children}</div>
         </div>

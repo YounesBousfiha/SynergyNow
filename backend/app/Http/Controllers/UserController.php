e9fdbc5a8db98de168d89d1729145434ab13e8f0
Coderyounes <younesanas2021@gmail.com>
2025-05-04 17:53:00 +0100

    Remove unused methods from UserController to clean up code

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -177,62 +177,62 @@ class UserController extends Controller
             ], 401);
         }
     }
-    public function updateName(UpdateRequestProfile $request)
-    {
-        $token = $request->bearerToken();
-
-        try {
-            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
-            $user = User::find($decoded->sub);
-
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 404);
-            }
-
-            $user->update($request->only(['firstname', 'lastname']));
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Unexpected Error'], 400);
-        }
-        return response()->json(['user' => $user]);
-    }
-
-    public function changeEmail(UpdateRequestEmail $request)
-    {
-        $token = $request->bearerToken();
-
-        try {
-            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
-            $user = User::find($decoded->sub);
-
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 404);
-            }
-
-            $user->update($request->only(['email']));
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Unexpected Error'], 400);
-        }
-        return response()->json(['user' => $user]);
-    }
-
-    // TODO: Upgrade the Password to confirm the user by it Password before deleting the account
-    public function deleteAccount(Request $request) {
-        $token = $request->bearerToken();
-
-        try {
-
-            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
-            $user = User::find($decoded->sub);
-
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 404);
-            }
-
-            $user->delete();
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Unexpected Error'], 400);
-        }
-
-        return response()->json(['message' => 'Account deleted']);
-    }
+//    public function updateName(UpdateRequestProfile $request)
+//    {
+//        $token = $request->bearerToken();
+//
+//        try {
+//            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+//            $user = User::find($decoded->sub);
+//
+//            if (!$user) {
+//                return response()->json(['error' => 'User not found'], 404);
+//            }
+//
+//            $user->update($request->only(['firstname', 'lastname']));
+//        } catch (\Exception $e) {
+//            return response()->json(['error' => 'Unexpected Error'], 400);
+//        }
+//        return response()->json(['user' => $user]);
+//    }
+//
+//    public function changeEmail(UpdateRequestEmail $request)
+//    {
+//        $token = $request->bearerToken();
+//
+//        try {
+//            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+//            $user = User::find($decoded->sub);
+//
+//            if (!$user) {
+//                return response()->json(['error' => 'User not found'], 404);
+//            }
+//
+//            $user->update($request->only(['email']));
+//        } catch (\Exception $e) {
+//            return response()->json(['error' => 'Unexpected Error'], 400);
+//        }
+//        return response()->json(['user' => $user]);
+//    }
+//
+//    // TODO: Upgrade the Password to confirm the user by it Password before deleting the account
+//    public function deleteAccount(Request $request) {
+//        $token = $request->bearerToken();
+//
+//        try {
+//
+//            $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+//            $user = User::find($decoded->sub);
+//
+//            if (!$user) {
+//                return response()->json(['error' => 'User not found'], 404);
+//            }
+//
+//            $user->delete();
+//        } catch (\Exception $e) {
+//            return response()->json(['error' => 'Unexpected Error'], 400);
+//        }
+//
+//        return response()->json(['message' => 'Account deleted']);
+//    }
 }

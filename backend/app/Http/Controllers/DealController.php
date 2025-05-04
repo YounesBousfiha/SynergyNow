42ef1413075e96713c883a540c26f43d1f0350f9
Coderyounes <younesanas2021@gmail.com>
2025-05-04 17:51:55 +0100

    Comment out updateStatus and unassign methods in DealController for future reference

--- backend/app/Http/Controllers/DealController.php
+++ backend/app/Http/Controllers/DealController.php
@@ -106,22 +106,22 @@ class DealController extends Controller
         ]);
     }
 
-    public function updateStatus(Request $request, string $dealId) {
-        $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
-        try {
-            $deal = Deal::where('my_companie_id', $companyId)->where('id', $dealId)->first();
-            if(!$deal) {
-                return response()->json(['message' => 'Deal not found'], 404);
-            }
-            $deal->update(['status' => $request->status]);
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
-
-        return response()->json([
-            'message' => $deal
-        ]);
-    }
+//    public function updateStatus(Request $request, string $dealId) {
+//        $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
+//        try {
+//            $deal = Deal::where('my_companie_id', $companyId)->where('id', $dealId)->first();
+//            if(!$deal) {
+//                return response()->json(['message' => 'Deal not found'], 404);
+//            }
+//            $deal->update(['status' => $request->status]);
+//        } catch (Exception $e) {
+//            return response()->json(['message' => $e->getMessage()], 400);
+//        }
+//
+//        return response()->json([
+//            'message' => $deal
+//        ]);
+//    }
     public function assign(Request $request, string $dealId) {
         $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
         try {
@@ -139,20 +139,20 @@ class DealController extends Controller
         ]);
     }
 
-    public function unassign(Request $request, string $dealId) {
-        $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
-        try {
-            $deal = Deal::where('my_companie_id', $companyId)->where('id', $dealId)->first();
-            if(!$deal) {
-                return response()->json(['message' => 'Deal not found'], 404);
-            }
-            $deal->update(['agent_id' => null]);
-        } catch (Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 400);
-        }
-
-        return response()->json([
-            'message' => $deal
-        ]);
-    }
+//    public function unassign(Request $request, string $dealId) {
+//        $companyId = AuthHelpers::getMyCompany($request->bearerToken())->id;
+//        try {
+//            $deal = Deal::where('my_companie_id', $companyId)->where('id', $dealId)->first();
+//            if(!$deal) {
+//                return response()->json(['message' => 'Deal not found'], 404);
+//            }
+//            $deal->update(['agent_id' => null]);
+//        } catch (Exception $e) {
+//            return response()->json(['message' => $e->getMessage()], 400);
+//        }
+//
+//        return response()->json([
+//            'message' => $deal
+//        ]);
+//    }
 }
